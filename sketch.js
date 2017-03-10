c151e2e81d33704319ec264d58ba159e6a4a8328
Andy <agarcia@wlapcs.org>
2017-03-10 16:23:08 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function setup(){
+    createCanvas(600,400);
+    background(200,200,200);
+}
+
+function draw(){
+    line(300,150,220,50);
+    line(355,150,440,40);
+    line(330,200,330,250);
+    line(280,250,380,250);
+
+    stroke(255,255,255);
+    fill(244,66,66);
+    rect(305,150,50,50);
+    rect(315,255,30,50)
+
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(255,255,255);
+    ellipse(380,120,40,40);
+    ellipse(280,120,40,40);
+    ellipse(380,120,10,10)
+    ellipse(280,120,10,10)
+    
+    
+}
+
